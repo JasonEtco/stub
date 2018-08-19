ea8fb9f81bf7545dbf1dcd9fe6ff7e0c01dbfafd
Jason Etcovitch <jasonetco@github.com>
2018-08-19 15:01:51 -0400

    Fix project name and template

--- bin/stub.js
+++ bin/stub.js
@@ -10,13 +10,17 @@ const kebabCase = require('lodash.kebabcase')
 const camelCase = require('lodash.camelcase')
 const chalk = require('chalk')
 const spawn = require('cross-spawn')
-const template = require('../package.json').repository.url
+const DEFAULT_TEMPLATE = 'https://github.com/JasonEtco/stub-template'
 
 program
   .usage('[options] [destination]')
   .option('-t, --title <title>', 'Title')
   .option('-d, --desc "<description>"', 'Description (contain in quotes)').parse(process.argv)
+  .option('-b, --branch <branch-name>', 'Specify a branch', 'master')
+  .option('--template <template-url>', 'URL or name of custom template', DEFAULT_TEMPLATE)
 
+if (!program.args.length) throw new Error('You must include a destination.')
+const project = program.args[program.args.length - 1]
 const destination = program.args.length
   ? path.resolve(process.cwd(), program.args.shift())
   : process.cwd()
@@ -24,12 +28,12 @@ const destination = program.args.length
 async function go (prompts) {
   const answers = await inquirer.prompt(prompts)
 
-  answers.project = kebabCase(destination)
-  answers.projectSlug = camelCase(destination)
+  answers.project = kebabCase(project)
+  answers.projectSlug = camelCase(project)
 
-  const results = await scaffold(template, destination, answers, {
+  const results = await scaffold(program.template, destination, answers, {
     overwrite: Boolean(program.overwrite),
-    branch: 'template'
+    branch: program.branch
   })
 
   results.forEach(fileinfo => {
@@ -54,7 +58,7 @@ go([
   {
     type: 'input',
     name: 'title',
-    default: () => kebabCase(path.basename(destination)),
+    default: () => kebabCase(project),
     message: 'Title:',
     when: !program.title
   }, {
